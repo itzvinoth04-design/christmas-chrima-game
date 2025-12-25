11782eb2dfa80bb9edbb4158950a9ba3b2a209d7
Vinoth <itz.vinoth04@gmail.com>
2025-12-25 12:47:41 +0530

    Disable debug in production

--- app.py
+++ app.py
@@ -243,4 +243,4 @@ with app.app_context():
 # ------------------ RUN ------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=False)
